06e1c8eff929ac7567732d21333f9da5a7ce97a5
Fernando Oliveros Ocampo <53822395+FernandoOliverosOcampo@users.noreply.github.com>
2023-09-26 13:53:12 -0500

    Modal comercio

--- js/vista/index.js
+++ js/vista/index.js
@@ -38,6 +38,64 @@ const Vista = {
         `;
     return modalContent;
   },
+  llenarModal2: function () {
+    const modal2 = document.getElementById("modal2");
+    const modalContent2 = modal2.querySelector(".modal-contenido2");
+
+    modalContent2.innerHTML = `
+            <div class="modal-cabecera2">
+                <div class="nombre">
+                    <p>Comercios</p>
+                </div
+              
+                <div class="cerrar">
+                    <span class="btn-cerrar-modal cerrar-modal-informacion" id="cerrarModal">&times;</span>
+                </div
+            </div>
+        
+            <div class="modal-cuerpo2">
+            <div class="titulo-comercios">
+                <h2>Comercios</h2>
+            </div>
+            <div class="comercios-modal">
+            <div class="tickets-contenedor">
+                            <div class="contenido-tickets">
+                                <div class="imagen-tickets">
+                                    <img src="./img/2.jpg" alt="">
+                                </div>
+                                <div class="tickets-contenido">
+                                    <div class="texto-ticket">
+                                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sequi unde beatae animi nostrum atque.</p>
+
+                                        <button><a href="#">20%</a></button>
+                                    </div>
+                                </div>
+    
+                            </div>
+                            <div class="tickets-contenedor">
+                            <div class="contenido-tickets">
+                                <div class="imagen-tickets">
+                                    <img src="./img/2.jpg" alt="">
+                                </div>
+                                <div class="tickets-contenido">
+                                    <div class="texto-ticket">
+                                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sit sequi unde beatae animi nostrum atque.</p>
+
+                                        <button><a href="#">20%</a></button>
+                                    </div>
+                                </div>
+    
+                            </div>
+       
+        </div>
+            </div>
+                
+            <div class="modal-pie2">
+          
+            </div>
+        `;
+    return modalContent2;
+  },
   abrirModal: function () {
     modal.style.display = "block";
     window.onclick = function (event) {
@@ -53,6 +111,21 @@ const Vista = {
       modal.style.display = "none";
     });
   },
+  abrirModal2: function () {
+    modal2.style.display = "block";
+    window.onclick = function (event) {
+      if (event.target == modal2) {
+        modal2.style.display = "none";
+      }
+    };
+  },
+
+  cerrarModal2: function () {
+    const botonCerrarModal = modal2.querySelector("#cerrarModal");
+    botonCerrarModal.addEventListener("click", () => {
+      modal2.style.display = "none";
+    });
+  },
   mostrarModal: function () {
     const botonAbrirModal = document.getElementById("ver-mas");
 
@@ -64,9 +137,23 @@ const Vista = {
       this.cerrarModal();
     });
   },
+  mostrarModal2: function () {
+    const botonAbrirModal = document.getElementById("comercio");
+
+    botonAbrirModal.addEventListener("click", () => {
+      // Abre el modal
+      this.abrirModal2();
+
+      // Obtén el botón de cerrar modal y agrega el evento de clic
+      this.cerrarModal2();
+    });
+  },
 };
 document.addEventListener('DOMContentLoaded', function () {
     Vista.llenarModal()
     Vista.mostrarModal()
+    Vista.llenarModal2()
+    Vista.mostrarModal2()
+   
   })
   
