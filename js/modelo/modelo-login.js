c1933a16ea124291b8dcbc1949e8533cc9a0a8f4
Fernando Oliveros Ocampo <53822395+FernandoOliverosOcampo@users.noreply.github.com>
2023-10-24 14:56:32 -0500

    Update modelo-login.js

--- js/modelo/modelo-login.js
+++ js/modelo/modelo-login.js
@@ -10,7 +10,7 @@ const Modelo = {
     try {
       const res = await axios({
         method: "POST",
-        url: ` http://127.0.0.1:5000/formulario`,
+        url: `http://foliveros.pythonanywhere.com/formulario`,
         data: datosIngresar,
       });
       return res;
